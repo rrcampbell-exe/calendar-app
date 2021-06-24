0ea6ab9cf3adab03a56ee6e77ae85270f4d9dc97
rrcampbell-exe <campbell.ryan.r@gmail.com>
2021-06-24 08:01:55 -0500

    Add unfocus functionality to textareas

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,22 +1,44 @@
+var tasks = {};
+
+// saving tasks to local storage
+var saveTasks = function () {
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+  };
+  
+
 // display date on page
 const currentDate = moment().format("dddd, MMMM, Do YYYY");
 const currentDateEl = document.querySelector("#currentDay");
 currentDateEl.textContent = currentDate;
 
 // editing task content fields
-
 $(".task-content").click(function () {
     // get current text of td element
     const text = $(this)
-      .text()
-      .trim();
-  
+        .text()
+        .trim();
+
     // replace td element with a new textarea
     const textInput = $("<textarea>")
-      .addClass("edit-task-content")
-      .val(text);
+        .addClass("edit-task-content")
+        .val(text);
     $(this).replaceWith(textInput);
-  
+
     // auto focus new element
     textInput.trigger("focus");
-  });
+});
+
+// editable task content field becomes unfocused
+$(document).on("blur", ".edit-task-content", function () {
+    // get current value of textarea
+    var text = $(this).val();
+
+    // recreate p element
+    var taskTd = $("<td>")
+        .addClass("task-content")
+        .text(text);
+
+    // replace textarea with new content
+    $(this).replaceWith(taskTd);
+});
+
