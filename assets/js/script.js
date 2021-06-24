9c036e389eda9dec682c133294369e7e13c5f126
rrcampbell-exe <campbell.ryan.r@gmail.com>
2021-06-24 06:24:01 -0500

    Make task content boxes editable

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,3 +2,21 @@
 const currentDate = moment().format("dddd, MMMM, Do YYYY");
 const currentDateEl = document.querySelector("#currentDay");
 currentDateEl.textContent = currentDate;
+
+// editing task content fields
+
+$(".task-content").click(function () {
+    // get current text of td element
+    const text = $(this)
+      .text()
+      .trim();
+  
+    // replace td element with a new textarea
+    const textInput = $("<textarea>")
+      .addClass("edit-task-content")
+      .val(text);
+    $(this).replaceWith(textInput);
+  
+    // auto focus new element
+    textInput.trigger("focus");
+  });
