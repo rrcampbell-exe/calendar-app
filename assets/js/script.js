bf1d3036e2f6cdb4ba196e97ddcac55b30312a51
rrcampbell-exe <campbell.ryan.r@gmail.com>
2021-06-25 06:18:21 -0500

    Update id assignment functionality to assign id to time-block based on for loop

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,11 @@
-var tasks = {};
+let tasks = {};
+const currentHour = moment().format("k");
 
 // saving tasks to local storage
-var saveTasks = function () {
+const saveTasks = function () {
     localStorage.setItem("tasks", JSON.stringify(tasks));
   };
   
-
 // display date on page
 const currentDate = moment().format("dddd, MMMM, Do YYYY");
 const currentDateEl = document.querySelector("#currentDay");
@@ -42,3 +42,46 @@ $(document).on("blur", ".edit-task-content", function () {
     $(this).replaceWith(taskTd);
 });
 
+// assign numerical id to hour of day
+let hourOfDay = document.querySelectorAll(".time-block")
+function hourOfDayIdAssign() {
+    hourOfDay.forEach(node => {
+        for (let i = 8; i < 18; i++) {
+            node.setAttribute("id", (i));
+            console.log(i);
+        }
+    })
+}
+
+// let hourOfDay = document.querySelectorAll(".time-block")
+// function hourOfDayIdAssign() {
+//     let hourOfDay = document.querySelectorAll(".time-block")
+//     // function hourOfDayIdAssign() {
+//     hourOfDay.forEach(node => {
+//         for (let i = 0; i < 9; i++) {
+//         node.setAttribute("id", (i + 9));
+//         console.log(i + 9);
+//     }
+// }
+    
+    
+//     // for (let i = 0; i < 9; i++) {
+//     //     hourOfDay.setAttribute("id", (i + 9));
+//     //     console.log(i + 9);
+        
+//     // }
+// };
+// assign background color to time row
+// helpful: https://stackoverflow.com/questions/4358155/changing-background-based-on-time-of-day-using-javascript
+// var hourOfDayId = hourOfDay.getAttribute("id").innerText;
+// if (document.body.hourOfDay) {
+//     if (currentHour > hourOfDay) {
+//         document.body.hourOfDayId.removeClass("present")
+//         document.body.hourOfDayId.addClass("past")
+//     } else if (currentHour < hourOfDay) {
+//         document.body.hourOfDayId.removeClass("present")
+//         document.body.hourOfDayId.addClass("future")
+//     }
+// }
+
+hourOfDayIdAssign();
