c6181b023ff9bb2d173cab14ec1a531d27f72d85
rrcampbell-exe <campbell.ryan.r@gmail.com>
2021-06-27 13:29:44 -0500

    Complete time of day functinoality

--- assets/js/script.js
+++ assets/js/script.js
@@ -60,23 +60,25 @@ $(document).on("blur", ".edit-task-content", function () {
 // helpful: https://stackoverflow.com/questions/4358155/changing-background-based-on-time-of-day-using-javascript
 // helpful with parseInt: https://www.w3schools.com/jsref/tryit.asp?filename=tryjsref_parseint
 function assignHourColor() {
-    let hourOfDay = document.querySelector(".time-block")
-    let hourOfDayId = document.getElementsByClassName("time-block")[1].id
-    let hourOfDayIdInt = parseInt(hourOfDayId);
-    let currentHourInt = parseInt(currentHour)
-    console.log(hourOfDayId);
-    console.log(hourOfDayIdInt);
-    console.log(currentHourInt);
-
-    if (hourOfDay) {
-        if (currentHourInt > hourOfDayIdInt) {
-            $(hourOfDay).removeClass("present")
-            $(hourOfDay).addClass("past")
-        } else if (currentHourInt < hourOfDayIdInt) {
-            $(hourOfDay).removeClass("present");
-            $(hourOfDay).addClass("future");
+    let hourOfDay = $(".time-block") // javascript DOM array
+    console.log(JSON.stringify(hourOfDay, null, 2))
+    console.log(hourOfDay[1])
+    for (let i = 1; i < 10; i++) {
+        let hourOfDayId = document.getElementsByClassName("time-block")[i].id
+        let hourOfDayIdInt = parseInt(hourOfDayId);
+        let currentHourInt = parseInt(currentHour)
+        console.log(hourOfDayIdInt);
+        console.log(currentHourInt);
+        if (hourOfDay) {
+            if (currentHourInt > hourOfDayIdInt) {
+                hourOfDay[i].setAttribute('class', 'time-block past')
+            } else if (currentHourInt < hourOfDayIdInt) {
+                hourOfDay[i].setAttribute('class', 'time-block future')
+            }
         }
+        
     }
+
 }
 
 function loadTasks() {
